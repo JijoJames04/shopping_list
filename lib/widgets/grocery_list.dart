ce4dfd1cc53deac29aa4ee6ca1758a886eb65c56
JijoJames04 <110347193+JijoJames04@users.noreply.github.com>
2023-06-14 23:13:27 +0530

    Sending DELETE Requests

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -81,10 +81,27 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https(
+      'flutter-prep-f5e9f-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+
+    final response = await http.delete(
+      url,
+    );
+
+    if (response.statusCode >= 400) {
+      // Optional: Show error message
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
